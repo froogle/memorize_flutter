985141eaf15a931038b29cb288c9f40f48d587e7
Pete Wright <peterjwright@gmail.com>
2021-10-30 16:49:27 +0100

    Starting to build out the Flutter version of the Memorize app from the Stanford CS193P course.

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Memorize - Flutter Edition'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: GridView.extent(
+            maxCrossAxisExtent: 150,
+            padding: const EdgeInsets.all(4),
+            mainAxisSpacing: 12,
+            crossAxisSpacing: 8,
+            childAspectRatio: 12 / 16,
+            children: _buildGridTileList(10)),
+      ),
+    );
+  }
+
+  var emojis = ["✈️", "🚀", "🚘", "🚎", "🚛", "🚐", "⛴", "🏍", "🚁", "🚂"];
+
+  List<Widget> _buildGridTileList(int count) => List.generate(
+      count,
+      (i) => Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.red, width: 4),
+              borderRadius: BorderRadius.circular(40),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: const Offset(0, 2))
+              ],
+            ),
+            child: Center(
+                child: Text(
+              emojis[i],
+              textScaleFactor: 4,
+            )),
+          ));
+}
