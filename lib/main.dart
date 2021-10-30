e9c723a54ec44c424b43133c1ec87a85b13e4a6a
Pete Wright <peterjwright@gmail.com>
2021-10-30 19:13:17 +0100

    Hooked up the provider to the main views, and cleaned up

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,17 @@ void main() {
 class EmojiGame extends ChangeNotifier {
   final emojis = ["✈️", "🚀", "🚘", "🚎", "🚛", "🚐", "⛴", "🏍", "🚁", "🚂"];
   final _deck = <String>[];
+  int _pairs = 0;
 
   UnmodifiableListView<String> get deck => UnmodifiableListView(_deck);
 
   EmojiGame(int numberOfPairs) {
-    final pairCount =
-        numberOfPairs > emojis.length ? emojis.length : numberOfPairs;
+    _pairs = numberOfPairs > emojis.length ? emojis.length : numberOfPairs;
 
     var shuffledEmojis = emojis;
     shuffledEmojis.shuffle();
 
-    for (var i = 0; i < pairCount; i++) {
+    for (var i = 0; i < _pairs; i++) {
       _deck.add(shuffledEmojis[i]);
       _deck.add(shuffledEmojis[i]);
     }
@@ -45,53 +45,73 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Memorize - Flutter Edition'),
+      home: const MainGameWidget(title: 'Memorize - Flutter Edition'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class MainGameWidget extends StatelessWidget {
+  const MainGameWidget({Key? key, required this.title}) : super(key: key);
   final String title;
-  final emojis = ["✈️", "🚀", "🚘", "🚎", "🚛", "🚐", "⛴", "🏍", "🚁", "🚂"];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: GridView.extent(
-            maxCrossAxisExtent: 150,
-            padding: const EdgeInsets.all(4),
-            mainAxisSpacing: 12,
-            crossAxisSpacing: 8,
-            childAspectRatio: 12 / 16,
-            children: _buildGridTileList(10)),
-      ),
+    return Consumer<EmojiGame>(
+      builder: (context, game, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Center(
+            child: cardList(game),
+          ),
+        );
+      },
     );
   }
 
-  List<Widget> _buildGridTileList(int count) => List.generate(
-      count,
-      (i) => Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.red, width: 4),
-              borderRadius: BorderRadius.circular(40),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 2,
-                    offset: const Offset(0, 2))
-              ],
-            ),
-            child: Center(
-                child: Text(
-              emojis[i],
-              textScaleFactor: 4,
-            )),
-          ));
+  GridView cardList(EmojiGame game) {
+    return GridView.extent(
+        maxCrossAxisExtent: 150,
+        padding: const EdgeInsets.all(4),
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 8,
+        childAspectRatio: 12 / 16,
+        children: _buildGridTileList(game.deck.length, game));
+  }
+
+  List<Widget> _buildGridTileList(int count, EmojiGame game) =>
+      List.generate(count, (i) => ExposedCard(cardFace: game.deck[i]));
+}
+
+class ExposedCard extends StatelessWidget {
+  const ExposedCard({
+    Key? key,
+    required this.cardFace,
+  }) : super(key: key);
+
+  final String cardFace;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.red, width: 4),
+        borderRadius: BorderRadius.circular(40),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 2,
+              offset: const Offset(0, 2))
+        ],
+      ),
+      child: Center(
+          child: Text(
+        cardFace,
+        textScaleFactor: 4,
+      )),
+    );
+  }
 }
